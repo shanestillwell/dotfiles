a5439346486fb3be1933c7c845c277c2dafd809e
Shane A. Stillwell <shane.stillwell@nexteraanalytics.com>
2023-12-27 19:07:47 -0700

    add telescope maps

--- dot_config/nvim/lua/custom/mappings.lua
+++ dot_config/nvim/lua/custom/mappings.lua
@@ -6,14 +6,22 @@ M.general = {
     ["jj"] = { "<ESC>" },
   },
   n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["Q"] = { "<cmd> q! <CR>", opts = { silent = true } },
     ["<leader>s"] = { "1z=", opts = { silent = true } },
     ["<C-p>"] = { "<cmd> Telescope find_files <CR>", "find files" },
+    ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "Find all" },
+    ["<leader>fu"] = { "<cmd> Telescope resume <CR>", "resume search" },
+    ["<leader>fs"] = { "<cmd> Telescope grep_string <CR>", "find files" },
     ["<leader>wk"] = { "<C-W>k", opts = { silent = true } },
     ["<leader>wj"] = { "<C-W>j", opts = { silent = true } },
     ["<leader>wh"] = { "<C-W>h", opts = { silent = true } },
     ["<leader>wl"] = { "<C-W>l", opts = { silent = true } },
+    -- ["<leader>fs"] = {
+    --   function()
+    --     require("telescope").builtin.grep_string()
+    --   end,
+    -- },
   },
   v = {
     ["<"] = { '<gv', 'Move VISUAL selection left'},
